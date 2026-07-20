89c12210261bd35f420e5b489d9afbd6bc146b90
Shayan <shayan.erfanian.sh@gmail.com>
2026-07-20 18:48:08 +0330

    contest

---
+++ .Contest/CodeForcces/Div3/1103_Div3/C_Omsk_Programmers.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int , int>
+#define pll pair<ll , ll>
+#define ln '\n'
+#define fo(i, n) for(int i = 0 ; i < n ; ++i)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define sz(x) (int)(x).size()
+#define pb(x) push_back(x)
+#define	set_dec(x) cout << fixed << setprecision(x);
+// 48-57 -> 0-9  65-90 -> A-Z 97-122 -> a-z
+const int dx[8] = {1, 0, -1, 0, 1, 1, -1, -1};
+const int dy[8] = {0, 1, 0, -1, -1, 1, -1, 1};
+int MOD = 1e9 + 7;
+const int N  = 2e5 + 10;
+const int INF = 2e9 + 100;
+const int MIN = -2e9 - 100;
+//always check input!!!
+
+void solve()
+{
+    int a , b , x;
+    cin >> a >> b >> x;
+    int ans = 0;
+    int cnt = INF;
+    while(a != b){
+        if(a < b) swap(a , b);
+        cnt = min(cnt , ans + a - b);
+        if(a - b == 1){
+            ans++;
+            break;
+        } 
+        if(a - b == 2 && a/x != b){
+            ans += 2;
+            break;
+        }
+        a /= x;
+        ans++;
+    }
+    cnt = min(cnt , ans + a - b);
+    cout << cnt << ln;
+}
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
